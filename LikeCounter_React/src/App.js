a9bffbdaec3eb112f7559e298f7b6581b13c2662
Ayush Pratap Singh <ayushpratap344@gmail.com>
2021-08-28 10:25:19 +0530

    Usage of useEffect Hook to display alert msg

--- LikeCounter_React/src/App.js
+++ LikeCounter_React/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
 import userEvent from '@testing-library/user-event';
@@ -22,3 +22,49 @@ function App() {
 }
 
 export default App;
+*/
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from "react";
+import userEvent from '@testing-library/user-event';
+import Color from 'color';
+
+
+function App() {
+  const [ctr, setctr] = useState(0);
+  const [ctrs, setctrs] = useState(0);
+  const [ctr3, setctr3] = useState(0);
+  useEffect(() => {
+    alert("you clicked");
+    console.log("clicxxx");
+  }, [ctr]);
+
+  function LikeclickHandler1() {
+    var newctr3 = ctr3 + 1;
+    setctr3(newctr3);
+  }
+
+
+
+
+    return (
+      <>
+        <h1>Like Counter by React-Hooks</h1>
+        <br />
+        <button onClick={() => {
+          setctr(ctr + 1);
+        }}>Like Me!!💕💕{ctr} </button>
+        <br />
+         <button onClick={LikeclickHandler1} >Like Me!!🌍🌍{ctr3} </button>
+         <br />
+        <button onClick={() => {
+          setctrs(ctrs + 1);
+        }}>Like Me!!🎉🎉{ctrs} </button>
+        <br />
+      </>
+    );
+  }
+
+  export default App;
+
+
